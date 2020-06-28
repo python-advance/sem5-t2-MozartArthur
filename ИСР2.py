636f1aa77e7e559ba6b7b8e22f0c083367e58e0a
MozartArthur <43474724+MozartArthur@users.noreply.github.com>
2020-06-29 02:59:32 +0300

    Add files via upload

---
+++ ИСР2.py
@@ -0,0 +1,18 @@
+# Создание программы, возвращающей список чисел Фибоначчи с помощью итератора.
+
+value = int(input("Введите максимальное число: \t" ))
+
+def fib(value):
+  lst = [0, 1, ]
+  it = iter(list(range(2, value)))
+  while True:
+    try:
+      elem = next(it)
+    except StopIteration:
+      break
+    n = lst[elem - 1] + lst[elem - 2]
+    lst.append(n)
+  return lst
+
+res = fib(value)
+print(res)
